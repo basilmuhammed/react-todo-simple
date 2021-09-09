32aabd9f02b24f66e32988c8d0c6bd9bbfce0429
Basil Muhamemd <basilmuhd@outlook.com>
2021-09-09 23:15:47 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          className="fas fa-plus"
+          onClick={() => setTodos([...todos, todo])}
+        ></i>
+      </div>
+      <div className="todos">
+        {todos.map((value, index) => {
+          return (
+            <div className="todo" key={index}>
+              <div className="left">
+                <input type="checkbox" name="" id="" />
+                <p>{value}</p>
+              </div>
+              <div className="right">
+                <i
+                  className="fas fa-times"
+                  onclick={() => {
+                    setTodos([todos.filter((index) => index !== index)]);
+                  }}
+                ></i>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
